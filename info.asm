48351b8f09c2fea7e1780a911ad71f1baa2525b1
dr2xmillion371 <119767559+dr2xmillion371@users.noreply.github.com>
2023-03-26 13:03:45 -0500

    info

---
+++ info.asm
@@ -0,0 +1,34 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Zsharp -- public domain
+;; Again feel free to use this
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+; a simple hello world
+
+
+section .text                   
+
+                           
+    global  _start          
+			                   
+
+_start:
+
+                                
+
+    mov     edx,len            
+    mov     ecx,msg            
+    mov     ebx,1               
+    mov     eax,4              
+    int     0x80            
+
+                                ;and exit
+
+  	mov     ebx,0               ;first syscall argument: exit code
+    mov     eax,1               ;system call number (sys_exit)
+    int     0x80                ;call kernel
+
+section .data                   ;section declaration
+
+msg db      "Hello, world!",0xa ;our dear string
+len equ     $ - msg             ;length of our dear string
