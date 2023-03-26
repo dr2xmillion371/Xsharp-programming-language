235bb5eee753215d75e5d838db69a02957b7113c
dr2xmillion371 <119767559+dr2xmillion371@users.noreply.github.com>
2023-03-26 12:54:44 -0500

    src

---
+++ src.py
@@ -0,0 +1,47 @@
+#--------------------------------------------
+# This is the official source code for Xsharp
+#--------------------------------------------
+
+
+
+#--------------------------------------------
+# Please note that this programming language
+# is not really one
+# it is a very small math interpreter instead
+# that prints out every number you type
+# so not a math interpreter
+#--------------------------------------------
+
+
+
+def info_text():
+  print('Zsharp, public domain')
+  print('Feel free to reuse this number displayer')
+  print('The reason why i call this a programming language')
+  print('is because it displays the language name as well as the number you typed in.')
+
+  
+  
+ 
+  def type_num():
+    users_number = input('xsharp > ')
+    print('Zsharp -- ' + users_number)
+    
+    
+#------------------------------------------
+# If you type a error, for example 1 / 0,
+# it will probably crash the program
+# -----------------------------------------
+
+
+
+
+
+#-----------------------------------------
+# Examples:
+# 56437
+# prints out: 56437
+# Last example:
+# 78574
+# prints out: 78574
+#----------------------------------------
